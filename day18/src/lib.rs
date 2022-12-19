6f6ed1153fbe3426d8bcaf818ed2e299bc5472fc
Alan Smith <alan@randomsmiths.com>
2022-12-19 09:14:46 -0500

    Changes after seeing someone else's solution

--- day18/src/lib.rs
+++ day18/src/lib.rs
@@ -14,64 +14,55 @@ pub struct Cube {
 
 #[derive(Debug)]
 pub struct Droplet {
-    cubes: Vec<Cube>,
+    cubes: HashSet<Cube>,
+}
+
+enum Dimension {
+    X,
+    Y,
+    Z,
 }
 
 impl Cube {
     pub fn new(x: i32, y: i32, z: i32) -> Self {
         Self { x, y, z }
     }
-    fn adjacent(&self, other: &Self) -> bool {
-        self.adjacent_x(other) || self.adjacent_y(other) || self.adjacent_z(other)
-    }
-    fn adjacent_x(&self, other: &Self) -> bool {
-        if self.y == other.y && self.z == other.z {
-            [-1, 1].into_iter().any(|d| self.x + d == other.x)
-        } else {
-            false
+    fn neighbors(&self) -> Vec<Cube> {
+        let mut neighbors = Vec::new();
+
+        for dimension in [Dimension::X, Dimension::Y, Dimension::Z] {
+            for offset in [-1, 1] {
+                let mut neighbor = self.clone();
+                match dimension {
+                    Dimension::X => neighbor.x += offset,
+                    Dimension::Y => neighbor.y += offset,
+                    Dimension::Z => neighbor.z += offset,
+                }
+                neighbors.push(neighbor);
+            }
         }
-    }
-    fn adjacent_y(&self, other: &Self) -> bool {
-        if self.x == other.x && self.z == other.z {
-            [-1, 1].into_iter().any(|d| self.y + d == other.y)
-        } else {
-            false
-        }
-    }
-    fn adjacent_z(&self, other: &Self) -> bool {
-        if self.x == other.x && self.y == other.y {
-            [-1, 1].into_iter().any(|d| self.z + d == other.z)
-        } else {
-            false
-        }
+        neighbors
     }
 }
 
 impl Droplet {
     pub fn new() -> Self {
-        Self { cubes: Vec::new() }
+        Self {
+            cubes: HashSet::new(),
+        }
     }
     pub fn add_line(&mut self, line: &String) {
         let result: IResult<&str, Vec<i32>> =
             separated_list1(tag(","), map_res(digit1, i32::from_str))(line);
         let numbers = result.unwrap().1;
         self.cubes
-            .push(Cube::new(numbers[0], numbers[1], numbers[2]));
+            .insert(Cube::new(numbers[0], numbers[1], numbers[2]));
     }
-    pub fn pt_1(&mut self) -> u32 {
-        let mut total = 0;
-        for cube in self.cubes.iter() {
-            let mut sides = 6;
-            for other_cube in self.cubes.iter() {
-                if cube == other_cube {
-                    continue;
-                }
-                if cube.adjacent(other_cube) {
-                    sides -= 1;
-                }
-            }
-            total += sides
-        }
-        total
+    pub fn pt_1(&mut self) -> usize {
+        self.cubes
+            .iter()
+            .flat_map(|cube| cube.neighbors())
+            .filter(|cube| !self.cubes.contains(cube))
+            .count()
     }
 }
